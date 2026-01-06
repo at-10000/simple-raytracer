350020289eb9c4dff2d04aca4ee447c150597dd9
andrei <andrei.tudor10000@gmail.com>
2026-01-06 23:10:12 +0200

    Added some more operators for Vec3, untested yet

--- src/vec3mod.rs
+++ src/vec3mod.rs
@@ -1,11 +1,11 @@
-use std :: ops :: {Add, Sub};
+use std :: ops :: {Add, Sub, Mul, Div};
 
 #[derive(Clone, Copy)]
 pub struct Vec3 {
     pub e: [f32; 3],
 }
 
-impl Vec3{
+impl Vec3 {
     pub fn new() -> Self {
         Self { e: [0.0; 3] }
     }
@@ -24,11 +24,11 @@ impl Vec3{
 
 }
 
-impl Add for Vec3 {
-    type Output = Self;
+impl Add<Vec3> for Vec3 {
+    type Output = Vec3;
 
-    fn add (self, other: Self) -> Self {
-        let mut result: Self = Vec3 :: new();
+    fn add (self, other: Self) -> Vec3 {
+        let mut result: Vec3 = Vec3 :: new();
         result.e[0] = self.x() + &other.x();
         result.e[1] = self.y() + &other.y();
         result.e[2] = self.z() + &other.z();
@@ -36,17 +36,53 @@ impl Add for Vec3 {
     }
 }
 
-impl Sub for Vec3 {
-    type Output = Self;
+impl Sub<Vec3> for Vec3 {
+    type Output = Vec3;
 
     fn sub (self, other: Self) -> Self {
-        let mut result: Self = Vec3 :: new();
+        let mut result: Vec3 = Vec3 :: new();
         result.e[0] = self.x() - &other.x();
         result.e[1] = self.y() - &other.y();
         result.e[2] = self.z() - &other.z();
         result
     }
 }
+
+
+impl Mul<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn mul (self, other: Vec3) -> Vec3 {
+        let mut result: Vec3 = Vec3 :: new();
+        result.e[0] = self.x() * &other.x();
+        result.e[1] = self.y() * &other.y();
+        result.e[2] = self.z() * &other.z();
+        result
+    }
+}
+
+
+impl Mul<f32> for Vec3 {
+    type Output = Vec3;
+
+    fn mul (self, other: f32) -> Vec3 {
+        let mut result: Vec3 = Vec3 :: new();
+        result.e[0] = self.e[0] * other;
+        result.e[1] = self.e[1] * other;
+        result.e[2] = self.e[2] * other;
+        result
+    }
+}
+
+impl Div<f32> for Vec3 {
+    type Output = Vec3;
+
+    fn div (self, other: f32) -> Vec3 {
+        self * (1.0 / other)
+    }
+}
+
+// pub fn dot () -> Vec3 {}
 
 
 
